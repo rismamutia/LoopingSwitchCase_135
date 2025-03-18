a8e7ef12e8d4d70ce3dabc1dcd0550e9fc6e28a9
rismamutia <rismamutiadewi89@gmail.com>
2025-03-18 20:28:40 +0700

    Revisi

--- file3.cpp
+++ file3.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
 
-float luasPersegiPanjang(float x, float y){
-    return x * y;
-}
 
-float luasLingkaran(float x){
-    return 3.14 * x * x;
-}
 
 int main (){
-    int pilihan;
-    float panjang,lebar,jejari;
+    
     do{
         cout << "===Menu===" << endl;
         cout << "1. Menghitung Luas Persegi Panjang" << endl;
