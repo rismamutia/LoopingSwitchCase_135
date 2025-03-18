d858ad33071bedb92b50a9274a53b55f74fc0c61
rismamutia <rismamutiadewi89@gmail.com>
2025-03-18 20:07:11 +0700

    Pembuatan Program Perulangan Do While

---
+++ file2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    //Perulangan While
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Perulangan While " << endl;
+    cout << "Nilai x awal = " << x << endl;
+
+    while(x < 7){
+        cout << "Nilai x = "<< x << " Lari Keliling " << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+
+    cout << "Nilai x terakhir = " << x << endl;
+    cout << "Jumlah Perulangan = " << perulanganWhile << endl;
+
+    //Perulangan Do While
+    cout << endl;
+    cout << "Perulangan Do While " << endl;
+    do{
+        cout << "Nilai x = "<< x << " Lari Keliling " << endl;
+        x = rand() % 10;
+        perulanganDo++;
+    }while(x < 7);
+    cout << "Nilai x terakhir : " << x << endl;
+    cout << "Jumlah Perulangan Do While : " << perulanganDo << endl;
+}
