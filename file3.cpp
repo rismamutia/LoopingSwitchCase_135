221824abd5e26dc7f6c8b6ea0bd698700e7e9cde
rismamutia <rismamutiadewi89@gmail.com>
2025-03-18 20:44:39 +0700

    Membuat Fungsi Luas Persegi Panjang dan Luas Lingkaran

--- file3.cpp
+++ file3.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+float luasPersegiPanjang(float x, float y){
+    return x * y;
+}
 
+float luasLingkaran(float x){
+    return 3.14 * x * x;
+}
 
 int main (){
     int pilihan;
